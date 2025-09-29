53b3899d0b475fe274937d73633ba2342f503422
Fi-ona10 <fiona.haeger@code.berlin>
2025-09-29 15:38:12 +0200

    updated edit blade (for error)

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,22 +1,43 @@
+
 <x-site-layout>
 
-    <form action="/articles/{{$article->id}}" method="post">
-
+    <form action="{{ route('articles.update', $article->id) }}" method="POST" novalidate>
         @method('PUT')
         @csrf
 
-        <div>
-            <label for="title">Title</label><br/>
-            <input type="text" name="title" class="bg-gray-200 p-2" value="{{$article->title}}">
+        <x-form-errors />
+
+        <div class="mb-4">
+            <label for="title" class="block font-semibold">Title</label>
+            <input
+                id="title"
+                type="text"
+                name="title"
+                value="{{ old('title', $article->title) }}"
+                class="bg-gray-200 p-2 w-full @error('title') border border-red-500 @enderror"
+                required
+            >
+            @error('title')
+                <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+            @enderror
         </div>
 
-        <div>
-            <label for="content">Content</label><br/>
-            <textarea name="content" class="bg-gray-200 p-2 w-3/4">{{$article->content}}</textarea>
+        <div class="mb-6">
+            <label for="content" class="block font-semibold">Content</label>
+            <textarea
+                id="content"
+                name="content"
+                class="bg-gray-200 p-2 w-3/4 h-40 @error('content') border border-red-500 @enderror"
+                required
+            >{{ old('content', $article->content) }}</textarea>
+            @error('content')
+                <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+            @enderror
         </div>
 
-        <br/><br/>
-
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">UPDATE</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+            Update
+        </button>
     </form>
+
 </x-site-layout>
