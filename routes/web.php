ea1af5b4593f162de422e310949b6c25eecfec82
Fi-ona10 <fiona.haeger@code.berlin>
2025-10-01 14:19:34 +0200

    Added route for WelcomeController to display the welcome view.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
