f1e13176c4fff44c7628f8bd38c2c8cd2c37e4d3
Fi-ona10 <fiona.haeger@code.berlin>
2025-10-01 19:14:17 +0200

    Refactor WelcomeController to conditionally fetch articles based on user authentication status

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,25 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        $articles = Article::latest()->take(5)->get(); // load 5 latest
+        if (Auth::check()) {
+            $articles = Article::where('user_id', Auth::id())->get();
+        } else {
+            $articles = Article::latest()->take(5)->get();
+        }
+
         return view('welcome', compact('articles'));
     }
-}
-
-use Illuminate\Support\Facades\Auth;
-
-public function index()
-{
-    if (Auth::check()) {
-        $articles = \App\Models\Article::where('user_id', Auth::id())->get();
-    } else {
-        $articles = \App\Models\Article::latest()->take(5)->get();
-    }
-
-    return view('welcome', compact('articles'));
 }
